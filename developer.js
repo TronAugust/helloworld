f5f48726c5b199cc7d354565c1ba60ae4763e955
buithetron <tronaugust@gmail.com>
2021-07-15 23:38:39 +0700

    hoc php nodejs

--- developer.js
+++ developer.js
@@ -1,1 +1,17 @@
-console.log("hi, i am tronj");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "tron05091999",
+  database: "mydb"
+});
+
+con.connect(function(err){
+  if(err) throw err;
+  con.query("select * from customers", function(err, result, fields){
+    if(err) throw err;
+    console.log(fields[2].name);
+  });
+});
